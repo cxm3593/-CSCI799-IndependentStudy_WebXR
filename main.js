cc14764030e0769d2bb6c3e49548690b4b26b6b5
cxm3593 <cxm3593@rit.edu>
2023-11-28 10:24:02 -0500

    New video page, star cloud, overlay...

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import { ARButton } from 'three/addons/webxr/ARButton.js';
 let camera, scene, renderer, mesh, mesh2, mesh3, mesh4, mesh5, mesh6;
 let controller;
 let arToolkitContext, arToolkitSource, markerControls;
+let pointCloud;
+let clock;
 
 
 
@@ -11,9 +13,14 @@ init();
 initObject();
 initARJS();
 initInteractiveControl();
+SetOverlays();
+// PostProcessing();
 animate();
 
+
 function init() {
+
+    clock = new THREE.Clock();
 
     const container = document.createElement( 'div' );
     document.body.appendChild( container );
@@ -68,16 +75,21 @@ function onWindowResize() {
 
 //
 
-function animate() {
-    
-    renderer.setAnimationLoop( render );
 
-}
 
 function render() {
+    var delta_time = clock.getDelta();
 
     arToolkitContext.update(arToolkitSource.domElement);
     scene.visible = camera.visible;
+    pointCloud.rotation.x -= 0.1 * delta_time;
+    pointCloud.rotation.z -= 0.1 * delta_time;
+    updateTextOverlay(mesh, "textOverlay1");
+    updateTextOverlay(mesh2, "textOverlay2");
+    updateTextOverlay(mesh3, "textOverlay3");
+    updateTextOverlay(mesh4, "textOverlay4");
+    updateTextOverlay(mesh5, "textOverlay5");
+    updateTextOverlay(mesh6, "textOverlay6");
     // console.log("Camera:", camera.position, camera.visible);
     // console.log("Mesh:", mesh.position, mesh.visible);
     renderer.render( scene, camera );
@@ -88,8 +100,8 @@ function initARJS() {
     //// arToolkitSource ////
     arToolkitSource = new THREEx.ArToolkitSource({
         sourceType : 'webcam',
-        sourceWidth: 1270,
-        sourceHeight: 720,
+        sourceWidth: 1920,
+        sourceHeight: 1080,
     })
 
     arToolkitSource.init(function onReady(){
@@ -163,7 +175,12 @@ function initARJS() {
     console.log('Pattern URL:', markerControls.parameters.patternUrl);
     console.log(markerControls);
     window.addEventListener('markerFound', function(ev){
-        console.log('Marker found!', ev);
+        // console.log('Marker found!', ev);
+        document.getElementById('overlay').style.display = 'block';
+    });
+
+    window.addEventListener('markerLost', function(ev){
+        document.getElementById('overlay').style.display = 'none';
     });
 
     scene.visible = true;
@@ -212,6 +229,8 @@ function initObject() {
         opacity: 0.5,
         shininess: 30, // Adjust shininess
         specular: 0x111111, // Color of specular highlights
+        side: THREE.DoubleSide,
+        depthTest : false,
     });
     mesh = new THREE.Mesh( geometry, material );
     mesh.interactive = true;
@@ -223,7 +242,9 @@ function initObject() {
         transparent: true,
         opacity: 0.5,
         shininess: 30, // Adjust shininess
-        specular: 0x111111, // Color of specular highlights
+        specular: 0x111111, // Color of specular highlights,
+        side: THREE.DoubleSide,
+        depthTest : false,
     });
     mesh2 = new THREE.Mesh( geometry, material2 );
     mesh2.url = 'video_page.html?videoUrl=' + encodeURIComponent('https://player.vimeo.com/video/888328899?h=1a18cdd048');
@@ -235,7 +256,9 @@ function initObject() {
         transparent: true,
         opacity: 0.5,
         shininess: 30, // Adjust shininess
-        specular: 0x111111, // Color of specular highlights
+        specular: 0x111111, // Color of specular highlights,
+        side: THREE.DoubleSide,
+        depthTest : false,
     });
     mesh3 = new THREE.Mesh( geometry, material3 );
     mesh3.url = 'video_page.html?videoUrl=' + encodeURIComponent('https://player.vimeo.com/video/888328787?h=3733b78225');
@@ -247,7 +270,9 @@ function initObject() {
         transparent: true,
         opacity: 0.5,
         shininess: 30, // Adjust shininess
-        specular: 0x111111, // Color of specular highlights
+        specular: 0x111111, // Color of specular highlights,
+        side: THREE.DoubleSide,
+        depthTest : false,
     });
     mesh4 = new THREE.Mesh( geometry, material4 );
     mesh4.url = 'video_page.html?videoUrl=' + encodeURIComponent('https://player.vimeo.com/video/888328829?h=f97ae1becb');
@@ -259,7 +284,9 @@ function initObject() {
         transparent: true,
         opacity: 0.5,
         shininess: 30, // Adjust shininess
-        specular: 0x111111, // Color of specular highlights
+        specular: 0x111111, // Color of specular highlights,
+        side: THREE.DoubleSide,
+        depthTest : false,
     });
     mesh5 = new THREE.Mesh( geometry, material5 );
     mesh5.url = 'video_page.html?videoUrl=' + encodeURIComponent('https://player.vimeo.com/video/888328847?h=4286ee4002');
@@ -271,7 +298,9 @@ function initObject() {
         transparent: true,
         opacity: 0.5,
         shininess: 30, // Adjust shininess
-        specular: 0x111111, // Color of specular highlights
+        specular: 0x111111, // Color of specular highlights,
+        side: THREE.DoubleSide,
+        depthTest : false,
     });
     mesh6 = new THREE.Mesh( geometry, material6 );
     mesh6.url = 'video_page.html?videoUrl=' + encodeURIComponent('https://player.vimeo.com/video/888328863?h=35d8d875d3');
@@ -288,4 +317,153 @@ function initObject() {
     scene.add(mesh5);
     scene.add(mesh6);
     // camera.add(mesh);
+
+    // Add thumbnails
+    async function requestThumbNail(video_id) {
+        const accessToken = "6db2c1db799b3defc219655861778459";
+        try {
+            const response = await fetch(`https://api.vimeo.com/videos/${video_id}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            console.log(data);
+            const thumbnailUrl = data.pictures.sizes[3].link;
+            console.log(thumbnailUrl);
+            return thumbnailUrl;
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    async function loadThumbnailAndCreatePlane(video_id, mesh, index) {
+        // const thumbnailUrl = await requestThumbNail(video_id);
+        // if (thumbnailUrl) {
+        //     const textureLoader = new THREE.TextureLoader();
+        //     const thumbnailTexture = textureLoader.load(thumbnailUrl);
+        // }
+        const textureLoader = new THREE.TextureLoader();
+        const thumbnailTexture = textureLoader.load("image/thumbnail" + String(index) + ".webp");
+        const planeMaterial = new THREE.MeshBasicMaterial({
+            map: thumbnailTexture,
+            side: THREE.DoubleSide,
+            transparent: true,
+            opacity: 0.7
+        });
+        const planeGeometry = new THREE.PlaneGeometry(0.3, 0.3);
+        const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+        planeMesh.position.set(0.01, 0, 0);
+        // Rotate the plane
+        const rotationInRadians = 90 * Math.PI / 180;
+        planeMesh.rotation.x = rotationInRadians;
+        planeMesh.rotation.y = rotationInRadians;
+        mesh.add(planeMesh);
+    }
+    
+
+    loadThumbnailAndCreatePlane("888328880", mesh, 1);
+    loadThumbnailAndCreatePlane("888328899", mesh2, 2);
+    loadThumbnailAndCreatePlane("888328787", mesh3, 3);
+    loadThumbnailAndCreatePlane("888328829", mesh4, 4);
+    loadThumbnailAndCreatePlane("888328847", mesh5, 5);
+    loadThumbnailAndCreatePlane("888328863", mesh6, 6);
+
+    // Add a visual point cloud
+    var ww = window.innerWidth,
+    wh = window.innerHeight;
+    var points = [];
+    var cloud_material = new THREE.PointsMaterial({
+        color: 0xffffff,
+        size:1,
+        blending: THREE.AdditiveBlending,
+        depthTest: false,
+    });
+
+    var cloud_geometry = new THREE.BufferGeometry();
+    var x, y, z;
+
+    for (var i = 0; i < 2000; i++) {
+        x = (Math.random() * ww * 2) - ww;
+        y = (Math.random() * wh * 2) - wh;
+        z = (Math.random() * 3000) - 1500;
+        
+        // Add the point as a flat array
+        points.push(x, y, z);
+    }
+
+    // Create a new Float32Array and use it as the 'position' attribute
+    cloud_geometry.setAttribute('position', new THREE.Float32BufferAttribute(points, 3));
+
+    pointCloud = new THREE.Points(cloud_geometry, cloud_material);
+    scene.add(pointCloud);
+
+
+}
+
+//// Add text overlay
+function addTextOverlay(text, x, y, text_name) {
+    const textDiv = document.createElement('div');
+    textDiv.id = text_name;
+    textDiv.style.position = 'absolute';
+    textDiv.style.color = 'white'; // Change as needed
+    textDiv.innerHTML = text;
+
+    document.getElementById('overlay').appendChild(textDiv);
+}
+
+function updateTextOverlay(mesh, text_name) {
+    const vector = new THREE.Vector3();
+    mesh.updateWorldMatrix(true, false);
+    mesh.getWorldPosition(vector);
+    vector.project(camera);
+
+    //const x = (vector.x *  .5 + .5) * renderer.domElement.clientWidth;
+    const x = (vector.x *  .5 + 0.18) * renderer.domElement.clientWidth;
+    const y = (vector.y *  -.5 + 0.5) * renderer.domElement.clientHeight;
+    // console.log(x, y);
+
+    const textOverlay = document.getElementById(text_name);
+    textOverlay.style.left = `${x}px`;
+    textOverlay.style.top = `${y}px`;
+    textOverlay.style.display = 'block'; // Show the text
+}
+
+function SetOverlays(){
+    addTextOverlay("Video 1", -1, -1, "textOverlay1");
+    addTextOverlay("Video 2", -1, -1, "textOverlay2");
+    addTextOverlay("Video 3", -1, -1, "textOverlay3");
+    addTextOverlay("Video 4", -1, -1, "textOverlay4");
+    addTextOverlay("Video 5", -1, -1, "textOverlay5");
+    addTextOverlay("Video 6", -1, -1, "textOverlay6");
+
+    const textDiv = document.createElement('div');
+    textDiv.id = "Hint";
+    textDiv.style.position = 'absolute';
+    textDiv.style.bottom = '50%';
+    textDiv.style.left = '50%';
+    textDiv.style.transform = 'translateX(-50%)';
+    textDiv.style.color = 'white'; // Change as needed
+    textDiv.innerHTML = "Scan the marker";
+
+    document.getElementById('overlay').appendChild(textDiv);
+}
+
+function PostProcessing(){
+    const composer = new THREE.EffectComposer(renderer);
+    const renderPass = new THREE.RenderPass(scene, camera);
+    composer.addPass(renderPass);
+
+    const bloomPass = new THREE.UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+    composer.addPass(bloomPass);
+
+    // In your render loop, replace renderer.render(scene, camera) with:
+    composer.render();
+}
+
+function animate() {
+    renderer.setAnimationLoop( render );
+
 }
